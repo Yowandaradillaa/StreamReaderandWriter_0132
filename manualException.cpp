7b09d942872e245a665250399d3f7df3db67f167
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-06-03 10:41:06 +0700

    Memasukan catch

--- manualException.cpp
+++ manualException.cpp
@@ -8,4 +8,11 @@ int main () {
         cout << "Selamat belajar di prodi TI UMY" << endl;
         cout << "Pernyataan tidak akan dieksekusi" << endl;
     }
+    catch (int a) {
+        cout << "Pegecualian akan dieksekusi" << endl;
+    }
+    catch (...) {
+        cout << "Default pengecualian dieksekusi" << endl;
+    }
+    return 0;
 }
