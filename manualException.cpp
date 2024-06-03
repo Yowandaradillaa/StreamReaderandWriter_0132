285d9a938633eea6ef759728e67097149c378443
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-06-03 10:33:48 +0700

    Memasukan try

--- manualException.cpp
+++ manualException.cpp
@@ -4,5 +4,8 @@
 using namespace std;
 
 int main () {
-    
+    try {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
 }
